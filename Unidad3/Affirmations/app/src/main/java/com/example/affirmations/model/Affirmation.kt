71acbc8239e3736f6935578e5d56ad601a5f992c
Martin Wilches <martindotdev@gmail.com>
2024-08-12 16:37:37 -0500

    Anotaciones de recursos dentro de una clase de datos

--- Unidad3/Affirmations/app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ Unidad3/Affirmations/app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -5,7 +5,7 @@ import androidx.annotation.StringRes
 
 // La clase de datos Affirmation contiene 2 propiedades val
 data class Affirmation(
-    // Las anotaciones representan que las propiedades son un ID de un recurso
+    // Las anotaciones @DrawableRes y @StringRes representan que las propiedades son un ID de un recurso
     @DrawableRes val imageResourceId: Int,
     @StringRes val stringResourceId: Int
 )
